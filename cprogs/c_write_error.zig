47dc04f9b4bb20c98345951616e049c53998c6df
Chris Boesch <48591413+chrboesch@users.noreply.github.com>
2023-02-17 18:37:06 +0100

    Update c_write_error.zig

--- cprogs/c_write_error.zig
+++ cprogs/c_write_error.zig
@@ -2,12 +2,10 @@ const std = @import("std");
 
 const c = @cImport({
     @cInclude("stdio.h");
-    @cInclude("unistd.h");
 });
 
 pub fn c_write_error() void {
     const fd = c.fdopen(2, "w");
-    // defer _ = c.close(2);
     defer _ = c.fclose(fd);
     const c_res = c.fprintf(fd, "This is an error!\n");
 
